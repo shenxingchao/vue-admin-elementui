f9efb29cd76233f3d08b03916131757b04617612
shenxingchao <864318466@qq.com>
2020-09-14 17:22:40 +0800

    1.筛选语言变量替换

--- src/components/CustomTable/index.vue
+++ src/components/CustomTable/index.vue
@@ -3,11 +3,11 @@
     <div class="block check-cloumn-container">
       <el-dropdown>
         <el-button type="primary" size="mini">
-          筛选<i class="el-icon-arrow-down el-icon--right"></i>
+          {{$t('opt.filter')}}<i class="el-icon-arrow-down el-icon--right"></i>
         </el-button>
         <el-dropdown-menu slot="dropdown">
           <el-checkbox v-model="checkAll" class="check-column-item" :indeterminate="isIndeterminate"
-                       @change="handleCheckAllChange">全选
+                       @change="handleCheckAllChange">{{$t('opt.check_all')}}
           </el-checkbox>
           <el-checkbox-group v-model="checkedColumn" @change="handleCheckedColumnChange">
             <div v-for="(item,index) in tableHead" :key="index">
